4d93faae170575e5ec848feb35c0e705be675432
Raitab <raitabplays@gmail.com>
2025-02-03 22:19:53 -0500

    Add l-wr-w's changes from https://github.com/Raitab/raid-data-tracker/pull/3. * Change backslashes to forward slashes for Unix compatibility * Refactor file paths
    Thank you, l-wr-w!

--- src/main/java/com/raidtracker/filereadwriter/FileReadWriter.java
+++ src/main/java/com/raidtracker/filereadwriter/FileReadWriter.java
@@ -12,7 +12,6 @@ import com.raidtracker.RaidTrackerItem;
 import com.raidtracker.RaidType;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -22,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static net.runelite.client.RuneLite.RUNELITE_DIR;
-
 
 @Slf4j
 public class FileReadWriter {
@@ -37,48 +35,39 @@ public class FileReadWriter {
 	@Inject
 	private Gson gson;
 
-	public void writeToFile(RaidTracker raidTracker)
-	{
-		String dir;
-
+    public void writeToFile(RaidTracker raidTracker) {
+        String fileName;
 		if (raidTracker.isInTheatreOfBlood()) {
-			dir = tobDir;
+            fileName = getRaidFileName(RaidType.TOB);
 		} else if (raidTracker.isInTombsOfAmascut()) {
-			dir = toaDir;
+            fileName = getRaidFileName(RaidType.TOA);
 		} else if (raidTracker.isInRaidChambers()) {
-			dir = coxDir;
+            fileName = getRaidFileName(RaidType.COX);
 		} else {
-			dir = defaultDir;
+            fileName = getRaidFileName(null);
 			log.warn("writeToFile called without an inRaid flag set.", new IllegalStateException());
 		}
 
-		try
-		{
+        try {
 			log.info("writer started");
 			//use json format so serializing and deserializing is easy
 			JsonParser parser = new JsonParser();
-			String fileName = dir + "\\raid_tracker_data.log";
 			FileWriter fw = new FileWriter(fileName,true); //the true will append the new data
 			gson.toJson(parser.parse(getJSONString(raidTracker, gson, parser)), fw);
 			fw.append("\n");
 			fw.close();
-		}
-		catch(IOException ioe)
-		{
+        } catch (IOException ioe) {
 			System.err.println("IOException: " + ioe.getMessage() + " in writeToFile");
 		}
 	}
 
-	public String getJSONString(RaidTracker raidTracker, Gson gson, JsonParser parser)
-	{
+    public String getJSONString(RaidTracker raidTracker, Gson gson, JsonParser parser) {
 		JsonObject RTJson =  parser.parse(gson.toJson(raidTracker)).getAsJsonObject();
-
 
 		List<RaidTrackerItem> lootList = raidTracker.getLootList();
 
 		//------------------ temporary fix until I can get gson.tojson to work for arraylist<RaidTrackerItem> ---------
 		JsonArray lootListToString = new JsonArray();
-
 
 		for (RaidTrackerItem item : lootList) {
 			lootListToString.add(parser.parse(gson.toJson(item, new TypeToken<RaidTrackerItem>() {
@@ -90,27 +79,22 @@ public class FileReadWriter {
 		//-------------------------------------------------------------------------------------------------------------
 
 		//massive bodge, works for now
-		return RTJson.toString().replace("\\\"", "\"").replace("\"[", "[").replace("]\"", "]");
+        return RTJson.toString().replace("\\\"", "\"").replace("\"[", "[").replace("]\"", "]");
 	}
 
-	public ArrayList<RaidTracker> readFromFile(String alternateFile, RaidType raidType)
-	{
-		String dir = getRaidDirectory(raidType);
+    public ArrayList<RaidTracker> readFromFile(String alternateFile, RaidType raidType) {
+        String fileName = getRaidFileName(raidType);
 
-		String fileName;
 		if (alternateFile.length() != 0) {
 			fileName = alternateFile;
-		} else {
-			fileName = dir + "\\raid_tracker_data.log";
 		}
 
 		try {
 			JsonParser parser = new JsonParser();
-
 			BufferedReader bufferedreader = new BufferedReader(new FileReader(fileName));
 			String line;
-
 			ArrayList<RaidTracker> RTList = new ArrayList<>();
+
 			while ((line = bufferedreader.readLine()) != null && line.length() > 0) {
 				try {
 					RaidTracker parsed = gson.fromJson(parser.parse(line), RaidTracker.class);
@@ -118,7 +102,6 @@ public class FileReadWriter {
 				} catch (JsonSyntaxException e) {
 					System.out.println("Bad line: " + line);
 				}
-
 			}
 
 			bufferedreader.close();
@@ -137,8 +120,7 @@ public class FileReadWriter {
 		return readFromFile("", raidType);
 	}
 
-	public void createFolders()
-	{
+    public void createFolders() {
 		File dir = new File(RUNELITE_DIR, "raid-data tracker");
 		IGNORE_RESULT(dir.mkdir());
 		dir = new File(dir, username);
@@ -151,24 +133,19 @@ public class FileReadWriter {
 		IGNORE_RESULT(dir_tob.mkdir());
 		IGNORE_RESULT(dir_toa.mkdir());
         IGNORE_RESULT(dir_default.mkdir());
-		File newCoxFile = new File(dir_cox + "\\raid_tracker_data.log");
-		File newTobFile = new File(dir_tob + "\\raid_tracker_data.log");
-		File newToaFile = new File(dir_toa + "\\raid_tracker_data.log");
-        File newDefaultFile = new File(dir_default + "\\raid_tracker_data.log");
-
-		try {
-			IGNORE_RESULT(newCoxFile.createNewFile());
-			IGNORE_RESULT(newTobFile.createNewFile());
-			IGNORE_RESULT(newToaFile.createNewFile());
-            IGNORE_RESULT(newDefaultFile.createNewFile());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
 		this.coxDir = dir_cox.getAbsolutePath();
 		this.tobDir = dir_tob.getAbsolutePath();
 		this.toaDir = dir_toa.getAbsolutePath();
         this.defaultDir = dir_default.getAbsolutePath();
+
+		try {
+            IGNORE_RESULT(new File(getRaidFileName(RaidType.COX)).createNewFile());
+            IGNORE_RESULT(new File(getRaidFileName(RaidType.TOB)).createNewFile());
+            IGNORE_RESULT(new File(getRaidFileName(RaidType.TOA)).createNewFile());
+            IGNORE_RESULT(new File(getRaidFileName(null)).createNewFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void updateUsername(final String username) {
@@ -180,13 +157,9 @@ public class FileReadWriter {
 	// Initial write made on reward chest interface opened,
 	// this updates after player leaves to account for purples/pets others receive
 	public void updateRTLog(RaidTracker raidTracker, RaidType raidType) {
-		String dir = getRaidDirectory(raidType);
-
 		try {
 			JsonParser parser = new JsonParser();
-
-			String fileName = dir + "\\raid_tracker_data.log";
-
+            String fileName = getRaidFileName(raidType);
 			ArrayList<RaidTracker> RTList = readFromFile(raidType);
 
 			FileWriter fw = new FileWriter(fileName, false); //the true will append the new data
@@ -210,23 +183,18 @@ public class FileReadWriter {
 	}
 
 	public void updateRTList(ArrayList<RaidTracker> RTList, RaidType raidType) {
-		String dir = getRaidDirectory(raidType);
-
 		try {
 			JsonParser parser = new JsonParser();
-
-			String fileName = dir + "\\raid_tracker_data.log";
-
-
+            String fileName = getRaidFileName(raidType);
 			FileWriter fw = new FileWriter(fileName, false); //the true will append the new data
 
 			for (RaidTracker RT : RTList) {
 				if (RT.getLootSplitPaid() > 0) {
 					RT.setSpecialLootInOwnName(true);
-				}
-				else {
+                } else {
 					//bit of a wonky check, so try to avoid with lootsplitpaid if possible
-					RT.setSpecialLootInOwnName(!RT.getLootList().isEmpty() && RT.getLootList().get(0).getName().equalsIgnoreCase(RT.getSpecialLoot()));
+                    RT.setSpecialLootInOwnName(!RT.getLootList().isEmpty()
+                            && RT.getLootList().get(0).getName().equalsIgnoreCase(RT.getSpecialLoot()));
 				}
 
 				gson.toJson(parser.parse(getJSONString(RT, gson, parser)), fw);
@@ -242,9 +210,7 @@ public class FileReadWriter {
 	}
 
 	public boolean delete(RaidType raidType) {
-		String dir = getRaidDirectory(raidType);
-
-		File newFile = new File(dir + "\\raid_tracker_data.log");
+        File newFile = new File(getRaidFileName(raidType));
 
 		boolean isDeleted = newFile.delete();
 
@@ -257,18 +223,25 @@ public class FileReadWriter {
 		return isDeleted;
 	}
 
-	public void IGNORE_RESULT(boolean b) {}
+    public void IGNORE_RESULT(boolean b) {
+    }
 
-	public String getRaidDirectory(RaidType raidType) {
+    public String getRaidFileName(RaidType raidType) {
+        final String DATA_FILE_NAME = "/raid_tracker_data.log";
+
+        if (raidType == null) {
+            return defaultDir + DATA_FILE_NAME;
+        }
+
 		switch(raidType) {
             case COX:
-                return coxDir;
+                return coxDir + DATA_FILE_NAME;
 			case TOB:
-				return tobDir;
+                return tobDir + DATA_FILE_NAME;
 			case TOA:
-				return toaDir;
+                return toaDir + DATA_FILE_NAME;
             default:
-                return defaultDir;
+                return defaultDir + DATA_FILE_NAME;
 		}
 	}
 }
